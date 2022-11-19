81d28a2e0add643dc219e664a6e792ea4bd9127f
ultrabot2022 <sayhellometaverse2022@gmail.com>
2022-11-19 12:13:08 -0800

    fix coat nft loading issue

--- src/utils/funcs.jsx
+++ src/utils/funcs.jsx
@@ -35,35 +35,35 @@ export const getInventoryBoxes = (inventoryItems) => {
 };
 
 export const getUserTokens = async ({ agent, user }) => {
-  return [
-    {
-      id: '1',
-      canister_id: "canister 1",
-      collection: "collection 1",
-      index: "1",
-      name: "token 1",
-      url: "assets/armor.png",
-      slot: 0,
-    },
-    {
-      id: '2',
-      canister_id: "canister 2",
-      collection: "collection 2",
-      index: "2",
-      name: "token 2",
-      url: "assets/bastard-sword.png",
-      slot: 1,
-    },
-    {
-      id: '3',
-      canister_id: "canister 3",
-      collection: "collection 3",
-      index: "3",
-      name: "token 3",
-      url: "models/chest.glb",
-      slot: 2,
-    },
-  ];
+  // return [
+  //   {
+  //     id: '1',
+  //     canister_id: "canister 1",
+  //     collection: "collection 1",
+  //     index: "1",
+  //     name: "token 1",
+  //     url: "assets/armor.png",
+  //     slot: 0,
+  //   },
+  //   {
+  //     id: '2',
+  //     canister_id: "canister 2",
+  //     collection: "collection 2",
+  //     index: "2",
+  //     name: "token 2",
+  //     url: "assets/bastard-sword.png",
+  //     slot: 1,
+  //   },
+  //   {
+  //     id: '3',
+  //     canister_id: "canister 3",
+  //     collection: "collection 3",
+  //     index: "3",
+  //     name: "token 3",
+  //     url: "models/chest.glb",
+  //     slot: 2,
+  //   },
+  // ];
 
   const collections = await getAllUserNFTs({
     agent,
@@ -76,24 +76,22 @@ export const getUserTokens = async ({ agent, user }) => {
   let slot = 0;
 
   collections.forEach((collection) => {
-    if (!collection.name.toLowerCase().includes("cipher")) {
-      collection.tokens.forEach((token) => {
-          let newToken = { id: slot.toString(), canister_id: token.canister, collection: token.collection, index: token.index.toString(), slot }
-          const jsonMetadata = token.metadata?.json?.value.TextContent;
+    collection.tokens.forEach((token) => {
+      let newToken = { id: slot.toString(), canister_id: token.canister, collection: token.collection, index: token.index.toString(), slot }
+      const jsonMetadata = token.metadata?.json?.value.TextContent;
 
-          if (jsonMetadata) {
-            const parseMetadata = JSON.parse(jsonMetadata);
-            newToken.name = parseMetadata.name
-            newToken.url = parseMetadata.animation_url ?? parseMetadata.image
-          } else {
-            newToken.name = token.collection
-            newToken.url = token.url
-          }
+      if (jsonMetadata) {
+        const parseMetadata = JSON.parse(jsonMetadata);
+        newToken.name = parseMetadata.name
+        newToken.url = parseMetadata.animation_url ?? parseMetadata.image
+      } else {
+        newToken.name = token.collection
+        newToken.url = token.url
+      }
 
-          newTokens[slot] = newToken
-          slot++;
-      });
-    }
+      newTokens[slot] = newToken
+      slot++;
+    });
   });
 
   console.log("newTokens: ", newTokens);
